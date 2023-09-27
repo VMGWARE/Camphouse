b45f30d57c62d09d448562ba74a7dba7eba569ce
Vahn Gomes <me@vahngomes.dev>
2023-09-27 13:07:25 -0400

    Refactor NotificationsView.vue to display notifications dynamically

--- frontend/src/views/NotificationsView.vue
+++ frontend/src/views/NotificationsView.vue
@@ -6,40 +6,30 @@
     <div class="card dark-card">
       <div class="card-body">
         <ul class="list-group dark-list">
-          <li class="list-group-item dark-item">
+          <li
+            class="list-group-item dark-item"
+            v-for="notification in notifications"
+            :key="notification.id"
+          >
             <div class="notification">
               <div class="notification-icon">
-                <i class="fa fa-envelope"></i>
+                <i
+                  class="fa fa-comment"
+                  v-if="notification.type === 'COMMENT'"
+                ></i>
+                <i class="fa fa-heart" v-if="notification.type === 'LIKE'"></i>
+                <i
+                  class="fa fa-envelope"
+                  v-if="notification.type === 'MESSAGE'"
+                ></i>
               </div>
               <div class="notification-details">
                 <p class="notification-message">
-                  You have a new message from John Doe
+                  {{ notification.message }}
                 </p>
-                <p class="notification-time">2 hours ago</p>
-              </div>
-            </div>
-          </li>
-          <li class="list-group-item dark-item">
-            <div class="notification">
-              <div class="notification-icon">
-                <i class="fa fa-thumbs-up"></i>
-              </div>
-              <div class="notification-details">
-                <p class="notification-message">Your post received 10 likes</p>
-                <p class="notification-time">1 day ago</p>
-              </div>
-            </div>
-          </li>
-          <li class="list-group-item dark-item">
-            <div class="notification">
-              <div class="notification-icon">
-                <i class="fa fa-comment"></i>
-              </div>
-              <div class="notification-details">
-                <p class="notification-message">
-                  Jane Smith commented on your post
+                <p class="notification-time">
+                  {{ new Date(notification.createdAt).toLocaleString() }}
                 </p>
-                <p class="notification-time">3 days ago</p>
               </div>
             </div>
           </li>
@@ -48,3 +38,34 @@
     </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "NotificationsView",
+  data() {
+    return {
+      notifications: [],
+    };
+  },
+  methods: {
+    async getNotifications() {
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${localStorage.getItem("token")}`;
+      await axios
+        .get("/v1/notifications")
+        .then((response) => {
+          this.notifications = response.data.data.notifications;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getNotifications();
+  },
+};
+</script>
