e7de1da742f1ef2737d6eee028457ba22815159f
Vahn Gomes <me@vahngomes.dev>
2023-10-17 11:09:05 -0400

    Added comments for content of audit logs page

--- frontend/src/views/Admin/AuditLogsView.vue
+++ frontend/src/views/Admin/AuditLogsView.vue
@@ -3,5 +3,11 @@
     <div class="d-flex justify-content-between align-items-center mb-4">
       <h1>Audit Logs</h1>
     </div>
+
+    <!-- Stats: Sign In, Logout, etc -->
+
+    <!-- Line chart that can filter between each action -->
+
+    <!-- Table of all audit logs -->
   </div>
 </template>
