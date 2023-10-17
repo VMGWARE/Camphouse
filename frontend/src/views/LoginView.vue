7a307731593ffca088308004eb2424c8e7a408ca
Vahn Gomes <me@vahngomes.dev>
2023-10-16 22:10:13 -0400

    Match class for div with sign up page

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row mt-3">
       <div class="col-md-6 offset-md-3">
         <div class="card">
           <div class="card-body">
