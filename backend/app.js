6937b7f21069e22bb80d99187d37a346448d6066
RollViral <51767087+tyler-Github@users.noreply.github.com>
2023-12-03 19:45:35 +0000

    Lock Cors to VmgWare domains
    Signed-off-by: RollViral <51767087+tyler-Github@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -57,7 +57,7 @@ app.use(Sentry.Handlers.tracingHandler());
 
 // Allow CORS
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://camphouse.vmgware.dev");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Forwarded-For"
