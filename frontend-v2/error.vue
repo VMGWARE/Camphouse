ff326e4cc26b41305a85e7c0fed64c3f1d175669
Vahn Gomes <me@vahngomes.dev>
2023-12-30 22:04:55 -0500

    Add error page component

---
+++ frontend-v2/error.vue
@@ -0,0 +1,30 @@
+<script>
+useSeoMeta({
+  title: "404 Not Found - Camphouse",
+  description:
+    "Oops! The page you are looking for does not exist. Please check the URL and try again.",
+});
+</script>
+
+<template>
+  <NuxtLayout>
+    <client-only>
+      <div class="container">
+        <div class="card">
+          <div class="card-body">
+            <h2 class="card-title">Page Not Found</h2>
+            <p>
+              The page you are looking for does not exist. Please check the URL
+              and try again.
+            </p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+              <router-link class="btn btn-primary" to="/">
+                <i class="fa fa-home"></i> Go back to home page.
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </client-only>
+  </NuxtLayout>
+</template>
