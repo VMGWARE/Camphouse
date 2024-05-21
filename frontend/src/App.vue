7208124eaf386dc06bb9f14432f69d50e94f6cf8
RollViral <51767087+tyler-Github@users.noreply.github.com>
2024-05-21 18:15:55 +0100

    Update App.vue (#201)
    Signed-off-by: RollViral <51767087+tyler-Github@users.noreply.github.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,24 +4,11 @@
       <router-view />
     </AdminLayout>
     <div v-else>
-      <nav
-        class="navbar navbar-expand-lg navbar-dark bg-dark mb-3 sticky-top"
-        style="padding-left: 20px"
-      >
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3 sticky-top" style="padding-left: 20px">
         <!-- Logo and Branding -->
         <router-link class="navbar-brand" to="/">
-          <img
-            src="@/assets/images/branding/Camphouse-v2.png"
-            alt="Camphouse Logo"
-            height="30"
-            width="30"
-          />
-          <img
-            src="@/assets/images/branding/Camphouse-Icon-Light.png"
-            alt="Camphouse Logo"
-            height="30"
-            width="150"
-          />
+          <img src="@/assets/images/branding/Camphouse-v2.png" alt="Camphouse Logo" height="30" width="30" />
+          <img src="@/assets/images/branding/Camphouse-Icon-Light.png" alt="Camphouse Logo" height="30" width="150" />
           <span class="badge alpha-badge" v-if="git.type == 'commit'">
             {{ git.version }}
           </span>
@@ -31,12 +18,7 @@
         </router-link>
 
         <!-- Navigation Toggler for Mobile -->
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-        >
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
           <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -44,80 +26,51 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto" v-if="!this.$store.state.loggedIn">
             <li class="nav-item">
-              <router-link class="nav-link" to="/login"
-                ><i class="fa fa-user"></i> Login</router-link
-              >
+              <router-link class="nav-link" to="/login"><i class="fa fa-user"></i> Login</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/signup"
-                ><i class="fa fa-user-plus"></i> Sign Up</router-link
-              >
+              <router-link class="nav-link" to="/signup"><i class="fa fa-user-plus"></i> Sign Up</router-link>
             </li>
             <li class="nav-item">
               <router-link class="nav-link" to="/about">
-                <i class="fa fa-info-circle"></i> About</router-link
-              >
+                <i class="fa fa-info-circle"></i> About</router-link>
             </li>
           </ul>
 
           <ul class="navbar-nav ms-auto" v-else>
             <li class="nav-item">
-              <router-link class="nav-link" to="/"
-                ><i class="fa fa-home"></i> Home</router-link
-              >
+              <router-link class="nav-link" to="/"><i class="fa fa-home"></i> Home</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/search"
-                ><i class="fa fa-search"></i> Search</router-link
-              >
+              <router-link class="nav-link" to="/search"><i class="fa fa-search"></i> Search</router-link>
             </li>
             <li class="nav-item">
-              <router-link
-                class="nav-link"
-                :to="'/@' + this.$store.state.user.handle"
-                ><i class="fa fa-user"></i> Profile</router-link
-              >
+              <router-link class="nav-link" :to="'/@' + this.$store.state.user.handle"><i class="fa fa-user"></i>
+                Profile</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/notifications"
-                ><i class="fa fa-bell"></i> Notifications</router-link
-              >
+              <router-link class="nav-link" to="/notifications"><i class="fa fa-bell"></i> Notifications</router-link>
             </li>
             <li class="nav-item">
               <!-- Dropdown for Additional Options -->
               <div class="dropdown">
-                <button
-                  class="nav-link btn btn-secondary dropdown-toggle px-3"
-                  type="button"
-                  id="userOptions"
-                  data-bs-toggle="dropdown"
-                >
+                <button class="nav-link btn btn-secondary dropdown-toggle px-3" type="button" id="userOptions"
+                  data-bs-toggle="dropdown">
                   Options
                 </button>
                 <div class="dropdown-menu dropdown-menu-end">
-                  <router-link
-                    class="dropdown-item"
-                    to="/admin"
-                    v-if="this.$store.state.user.admin"
-                    ><i class="fa fa-user-shield"> </i> Admin</router-link
-                  >
-                  <router-link class="dropdown-item" to="/settings"
-                    ><i class="fa fa-cog"></i> Settings</router-link
-                  >
-                  <p
-                    class="fake-link dropdown-item"
-                    @click="logout"
-                    v-if="!isLoading"
-                  >
+                  <router-link class="dropdown-item" to="/admin" v-if="this.$store.state.user.admin"><i
+                      class="fa fa-user-shield"> </i> Admin</router-link>
+                  <router-link class="dropdown-item" to="/settings"><i class="fa fa-cog"></i> Settings</router-link>
+                  <p class="fake-link dropdown-item" @click="logout" v-if="!isLoading">
                     <i class="fas fa-sign-out-alt"></i> Logout
                   </p>
                 </div>
               </div>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link btn btn-secondary" to="/new-post"
-                ><i class="fa fa-plus"></i> New Post</router-link
-              >
+              <router-link class="nav-link btn btn-secondary" to="/new-post"><i class="fa fa-plus"></i> New
+                Post</router-link>
             </li>
           </ul>
         </div>
@@ -158,7 +111,10 @@ export default {
 
         // Define public paths and patterns
         const publicPaths = ["/login", "/signup", "/about", "/"];
-        const publicPathPatterns = [/^\/@[^/]+$/, /^\/post\/[^/]+$/];
+        const publicPathPatterns = [
+          /^\/@[^/]+$/, // Matches paths like /@RollViral
+          /^\/post\/[^/]+$/ // Matches paths like /post/65faff92ebc47fed0299246c
+        ];
 
         // Check if the current path is public
         const isPublicPath =
@@ -168,12 +124,10 @@ export default {
         // If redirect does not contain a public path, redirect to login
         if (!isPublicPath) {
           redirect = "/login?redirect=" + redirect;
-        }
-
-        // Redirect to login page if needed
-        if (!isPublicPath || !publicPaths.includes(redirect)) {
           this.$router.push(redirect);
         }
+
+        // Return here to prevent further execution
         return;
       }
       let exp;
@@ -302,30 +256,48 @@ export default {
 .fake-link {
   cursor: pointer;
 }
+
 #main {
   min-height: 100%;
   /* overflow: auto; */
 }
+
 .alpha-badge {
-  background-color: #ff4500; /* orange-red color */
-  color: #ffffff; /* white text color */
-  padding: 2px 8px; /* some padding for spacing */
-  border-radius: 4px; /* rounded corners */
-  font-size: 0.8em; /* smaller font size than regular text */
-  font-weight: bold; /* bold text */
-  vertical-align: middle; /* align it with the logo and site name */
-  margin-left: 5px; /* spacing from the logo or site name */
+  background-color: #ff4500;
+  /* orange-red color */
+  color: #ffffff;
+  /* white text color */
+  padding: 2px 8px;
+  /* some padding for spacing */
+  border-radius: 4px;
+  /* rounded corners */
+  font-size: 0.8em;
+  /* smaller font size than regular text */
+  font-weight: bold;
+  /* bold text */
+  vertical-align: middle;
+  /* align it with the logo and site name */
+  margin-left: 5px;
+  /* spacing from the logo or site name */
 }
 
 .production-badge {
-  background-color: #00ff00; /* green color */
-  color: #ffffff; /* white text color */
-  padding: 2px 8px; /* some padding for spacing */
-  border-radius: 4px; /* rounded corners */
-  font-size: 0.8em; /* smaller font size than regular text */
-  font-weight: bold; /* bold text */
-  vertical-align: middle; /* align it with the logo and site name */
-  margin-left: 5px; /* spacing from the logo or site name */
+  background-color: #00ff00;
+  /* green color */
+  color: #ffffff;
+  /* white text color */
+  padding: 2px 8px;
+  /* some padding for spacing */
+  border-radius: 4px;
+  /* rounded corners */
+  font-size: 0.8em;
+  /* smaller font size than regular text */
+  font-weight: bold;
+  /* bold text */
+  vertical-align: middle;
+  /* align it with the logo and site name */
+  margin-left: 5px;
+  /* spacing from the logo or site name */
 }
 
 .badge {
